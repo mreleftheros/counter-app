52f74542c0e115140351ac7b136b73dab1ac2212
eleftheros <eleftheros_all@protonmail.com>
2021-05-28 21:13:55 +0300

    Finish project

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,36 @@
-console.log("hello world");
+const buttons = document.querySelector(".buttons");
+const counter = document.getElementById("counter");
+let counterValue = +counter.textContent;
+
+const updateHTML = (color, value) => {
+  counter.style.color = color;
+  counter.textContent = value;
+};
+
+const checkCounter = value => {
+  let color;
+
+  if(value > 0)
+    color = "#0f0";
+  else if(value < 0)
+    color = "#f00";
+  else 
+    color = "#fff";
+
+  updateHTML(color, value);
+};
+
+buttons.addEventListener("click", e => {
+  let text = e.target.innerText;
+
+  if(text === "Subtract")
+    counterValue--;
+
+  if(text === "Reset")
+    counterValue = 0;
+
+  if(text === "Add")
+    counterValue++;
+
+  checkCounter(counterValue);
+})
